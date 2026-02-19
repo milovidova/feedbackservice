5a4a98c913fbb4918949a83cee3f610e3778b0ec
milovidova <145771799+milovidova@users.noreply.github.com>
2026-02-19 03:43:42 +0300

    apiii

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,39 +1,14 @@
-# class ProjectsController < ApplicationController
-#   def index
-#     @projects = Project.all
-#   end
-
-#   def new
-#     @project = Project.new
-#   end
-
-#   def create
-#     @project = Project.new(project_params)
-#     @project.user = User.first 
-    
-#     if @project.save
-#       redirect_to projects_path, notice: 'Проект создан!'
-#     else
-#       render :new
-#     end
-#   end
-
-#   private
-
-#   def project_params
-#     params.require(:project).permit(:title, :description, :category, :feedback_request, :image)
-#   end
-# end
-
-
-
 class ProjectsController < ApplicationController
 
   def index
     @projects = Project.all
     @view_mode = :gallery  
-  end
 
+    respond_to do |format|
+      format.html
+      format.json { render json: @projects }
+    end
+  end
 
   def grid
     @projects = Project.includes(:user).all
@@ -41,41 +16,43 @@ class ProjectsController < ApplicationController
     render :index 
   end
 
-
   def show
     @project = Project.includes(feedbacks: :user).find(params[:id])
-  end
 
+    respond_to do |format|
+      format.html
+      format.json { render json: @project }
+    end
+  end
 
   def new
     @project = Project.new
   end
 
-
   def create
     @project = Project.new(project_params)
-    @project.user = User.first 
-    
-    if @project.save
-      redirect_to project_path(@project), notice: 'Проект создан!'
-    else
-      render :new
+    @project.user = User.first
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to project_path(@project), notice: "Проект создан!" }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
     end
   end
 
- 
   def destroy
     @project = Project.find(params[:id])
     @project.destroy
-    
     redirect_to projects_path, notice: 'Проект успешно удален'
   end
 
- 
   def edit
     @project = Project.find(params[:id])
   end
-
 
   def update
     @project = Project.find(params[:id])
